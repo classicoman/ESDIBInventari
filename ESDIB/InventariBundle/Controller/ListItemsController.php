9f7c67e54f4b6f9d61d163c1efa1cdb2666b571a
tamengual9 <toni_amengual@yahoo.es>
2014-01-15 16:03:44 +0100

    Taula Proveidors + primeres Templates complexes

--- ESDIB/InventariBundle/Controller/ListItemsController.php
+++ ESDIB/InventariBundle/Controller/ListItemsController.php
@@ -7,23 +7,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class ListItemsController extends Controller
 {
 	/* Funció */
-    public function showAction()
-    {
+    public function showAction($max_rows)
+    {     
+                //Montam el Select: només 1 dels elements de l'array resultant
+                //activarà un valor del <select>
+                $select_rows = array();
+                $values = array('5','10','15','20');
+                foreach ($values as $v) {
+                    $select_rows = ($v == $max_rows) ? "selected='selected'" : "";
+                }
+                
+                
+                //Query
 		$items = $this->getDoctrine()
 		   ->getRepository('ESDIBInventariBundle:Item')
 		   ->findAll();
-		   
 		if (!$items) {
 		    throw $this->createNotFoundException(
 			    'No hi ha cap item a dins de la taula'
 			);
 		}
 		
-		//Pinta la Plantilla amb els Items
+		//Redner Template
 		return $this->render('ESDIBInventariBundle:Items:items.html.twig', 
 			array('files' => $items,
 			'max_rows' => 5, 
-			'rotuls' => array('id','nom','preu') 
+			'rotuls' => array('id','nom','preu'),
+                        'select_rows' => $select_rows
 		));
 	}
 }
